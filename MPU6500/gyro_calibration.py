7217f6376e6a66d212807032ff48117c6ad8c177
dima5654 <69797810+dima5654@users.noreply.github.com>
2025-06-24 23:40:37 +0200

    Update gyro_calibration.py

--- MPU6500/gyro_calibration.py
+++ MPU6500/gyro_calibration.py
@@ -1,25 +1,49 @@
+# Shows Pi is on by turning on LED when plugged in
 from machine import Pin, I2C
 from time import sleep
-from mpu6500 import MPU6500  # Make sure this matches your file
+from mpu6500 import MPU6500  # Make sure your imu.py has MPU6500 defined
+import time
 
-# Turn on LED (optional)
-# I2C config
-i2c = I2C(0, scl=Pin(22), sda=Pin(21), freq=400000)
+
+# I2C configuration
+i2c = I2C(0, scl=Pin(22), sda=Pin(21), freq=400000)  # Adjust SDA/SCL pins as needed
 imu = MPU6500(i2c)
+t0 = time.time()
 
-# Your calculated offsets
-gx_offset = 0.01063169
-gy_offset = -0.01223756
-gz_offset = -0.00640248
+settling_time = 4
 
-while True:
+print('Settling MPU for %d sconds' % settling_time)
+time.sleep(4)
+print('MPU is done settling')
+# Loop to read and print accelerometer values
+def get_gyro():
     gx, gy, gz = imu.gyro
+    return gx, gy, gz
 
-    # Apply offsets
-    gx -= gx_offset
-    gy -= gy_offset
-    gz -= gz_offset
 
-    print("gx:", gx, "\t", "gy:", gy, "\t", "gz:", gz)
-    sleep(0.2)
+def gyro_calibration(calibration_time=20):
+    print('--' * 25)
+    print('Beginning of calibration, do not move the sensor')
+    
+    offsets = [0, 0, 0]
+    
+    num_of_points = 0
+    
+    end_loop_time = time.time() + calibration_time
+    
+    while end_loop_time > time.time():
+        num_of_points += 1
+        (gx, gy, gz) = get_gyro()
+        offsets[0] += gx
+        offsets[1] += gy
+        offsets[2] += gz
+        
+        if num_of_points % 100 == 0:
+            print('Still calibration... %d points so far' % num_of_points)
+    print('Calibration complete %d total points' % num_of_points)
+    offsets = [i/num_of_points for i in offsets]
+    return offsets
+
+print(gyro_calibration())
+
 
