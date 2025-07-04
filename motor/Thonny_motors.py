45b40b2bb409cbc17eb06e16922d819d287c147c
dima5654 <69797810+dima5654@users.noreply.github.com>
2025-07-04 23:45:32 +0200

    Update Thonny_motors.py

--- motor/Thonny_motors.py
+++ motor/Thonny_motors.py
@@ -1,23 +1,38 @@
 from machine import Pin, PWM
+import time
 
 # Set up PWM for motor A
-pwmA1 = PWM(Pin(18), freq=1000)
-pwmA2 = PWM(Pin(19), freq=1000)
+pwmA1 = PWM(Pin(33), freq=1000)
+pwmA2 = PWM(Pin(32), freq=1000)
 
 # Set up PWM for motor B
-pwmB1 = PWM(Pin(12), freq=1000)
-pwmB2 = PWM(Pin(13), freq=1000)
+pwmB1 = PWM(Pin(25), freq=1000)
+pwmB2 = PWM(Pin(26), freq=1000)
 
-def motor_A_forward(speed):
+def left_motor_forward(speed):
     pwmA1.duty(int(speed))   # speed: 0 to 1023
     pwmA2.duty(0)
+    
+def left_motor_backward(speed):
+    pwmA1.duty(0)   # speed: 0 to 1023
+    pwmA2.duty(int(speed))
 
-def motor_B_forward(speed):
+def right_motor_forward(speed):
     pwmB1.duty(int(speed))
     pwmB2.duty(0)
+    
+def right_motor_backward(speed):
+    pwmB1.duty(0)
+    pwmB2.duty(int(speed))
 
 def stop_motors():
     pwmA1.duty(0)
     pwmA2.duty(0)
     pwmB1.duty(0)
     pwmB2.duty(0)
+
+#while True:
+ #   left_motor_forward(0)
+  #  right_motor_forward(570)
+   # print('lol')
+    #time.sleep(1)
